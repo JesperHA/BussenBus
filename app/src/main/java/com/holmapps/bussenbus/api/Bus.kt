72139d6e76b8813d7992d4dcbfb236740e39c47a
Ulrik Holm <ulrikholmuh@gmail.com>
2020-09-15 09:13:10 +0200

    added Route classes and deserializer

--- app/src/main/java/com/holmapps/bussenbus/api/Bus.kt
+++ app/src/main/java/com/holmapps/bussenbus/api/Bus.kt
@@ -4,7 +4,18 @@ data class Bus (
     val title: String,
     val longtitude: Double,
     val latitude: Double,
+    val id: String,
     val nextStop: String,
     val icon: Int,
     val delayInMins: String
+)
+
+data class Coordinate (
+    val longtitude: Double,
+    val latitude: Double
+)
+
+data class BusStop (
+    val coordinate: Coordinate,
+    val title:String
 )
--- app/src/main/java/com/holmapps/bussenbus/api/BusResponse.kt
+++ app/src/main/java/com/holmapps/bussenbus/api/BusResponse.kt
@@ -1,3 +1,5 @@
 package com.holmapps.bussenbus.api
 
 data class BusResponse(val busList: List<Bus>)
+
+data class RouteResponse(val routeList: List<Coordinate>, val stopsList: List<BusStop>)
