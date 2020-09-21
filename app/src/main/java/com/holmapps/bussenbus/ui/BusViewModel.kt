48525b1bb9c06015f5c027230fe0135446a4830a
iosefstalin <jelleholm91@gmail.com>
2020-09-21 14:25:43 +0200

    major refactoring done, helper class added

--- app/src/main/java/com/holmapps/bussenbus/ui/BusViewModel.kt
+++ app/src/main/java/com/holmapps/bussenbus/ui/BusViewModel.kt
@@ -1,8 +1,16 @@
 package com.holmapps.bussenbus.ui
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.holmapps.bussenbus.R
 import com.holmapps.bussenbus.api.Bus
 import com.holmapps.bussenbus.repository.BusRepository
 import com.holmapps.bussenbus.repository.RouteObject
@@ -37,4 +45,81 @@ class BusViewModel @Inject constructor(private val repository: BusRepository) : 
             Timber.i("Locations Fetched")
         }
     }
+
+    fun getMarkerIcon(context: Context, bus: Bus, zoomLevel: Float): BitmapDescriptor? {
+        val markerView =
+            CustomMarkerView(
+                context,
+                bus,
+                zoomLevel,
+                ::getColor
+            )
+        return BitmapDescriptorFactory.fromBitmap(getBitmapFromView(markerView))
+    }
+
+    fun getBitmapFromView(view: View): Bitmap? {
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+        var bitmap =
+            Bitmap.createBitmap(view.measuredWidth, view.measuredHeight, Bitmap.Config.ARGB_8888)
+        var canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+
+    fun getColor(context: Context, id: String): Int {
+
+        return when (id) {
+            "1" -> ContextCompat.getColor(context, R.color.Bus1and4)
+            "2" -> ContextCompat.getColor(context, R.color.Bus2)
+            "3" -> ContextCompat.getColor(context, R.color.Bus3and5)
+            "4" -> ContextCompat.getColor(context, R.color.Bus1and4)
+            "5" -> ContextCompat.getColor(context, R.color.Bus3and5)
+            "6" -> ContextCompat.getColor(context, R.color.Bus6)
+            "7" -> ContextCompat.getColor(context, R.color.Bus7and8)
+            "8" -> ContextCompat.getColor(context, R.color.Bus7and8)
+            "9" -> ContextCompat.getColor(context, R.color.Bus9)
+            "10" -> ContextCompat.getColor(context, R.color.Bus10)
+            "Færge" -> ContextCompat.getColor(context, R.color.Boat)
+            else -> ContextCompat.getColor(context, R.color.standard)
+        }
+    }
+
+    fun busInfoGenerator(busList: List<Bus>, tag: String): String {
+        val builder = StringBuilder()
+        var result = ""
+        if (!busList.isEmpty()) {
+            busList.forEach { bus ->
+                if (bus.id == tag) {
+                    builder.append("Bus ").append(bus.title).append(" mod ").append(bus.destination)
+                        .append("\nNæste stop: ").append(bus.nextStop)
+                        .append(
+                            if (bus.delayInMins == "0" || bus.delayInMins.isEmpty()) {
+                                "\nKører til tiden"
+                            } else {
+                                "\nForsinkelse: "
+                            }
+                        ).append(
+                            if (bus.delayInMins == "0" || bus.delayInMins.isEmpty()) {
+                                ""
+                            } else {
+                                bus.delayInMins
+                            }
+                        ).append(
+                            if (bus.delayInMins == "0" || bus.delayInMins.isEmpty()) {
+                                ""
+                            } else if (bus.delayInMins == "1") {
+                                " Minut"
+                            } else {
+                                " Minutter"
+                            }
+
+                        )
+                    result = builder.toString()
+                }
+            }
+        }
+        return result
+    }
+
 }
--- app/src/main/java/com/holmapps/bussenbus/ui/MapFragment.kt
+++ app/src/main/java/com/holmapps/bussenbus/ui/MapFragment.kt
@@ -1,29 +1,20 @@
 package com.holmapps.bussenbus.ui
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.RelativeLayout
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.*
-import com.holmapps.bussenbus.R
 import com.holmapps.bussenbus.api.Bus
 import com.holmapps.bussenbus.databinding.MapFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -45,6 +36,8 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private val uiScope = CoroutineScope(Main)
     private lateinit var polyLine: Polyline
     lateinit var busList: List<Bus>
+    private val updateRateInMillis: Long = 1000
+    private val allMarkers = mutableListOf<Marker>()
 
     companion object {
         fun newInstance() = MapFragment()
@@ -65,100 +58,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val binding = MapFragmentBinding.bind(view)
-
     }
-
-    // Putting markers in list to enable removal of markerrs without clearing entire map
-    private val allMarkers = mutableListOf<Marker>()
-    private val updateRateInMillis: Long = 1000
-
-
-    private suspend fun plotBusMarkers(busList: List<Bus>, zoomLevel: Float) {
-
-
-        if (busList.isEmpty()) {
-            bus_info.visibility = View.INVISIBLE
-            if (polylineExist) {
-                polyLine.remove()
-            }
-        }
-
-        if (busList.isNotEmpty()) {
-            for (i in 0 until busList[0].coordinatList.size() - 1) {
-//            delay(500)
-
-                allMarkers.forEach { marker ->
-                    marker.remove()
-                }
-                allMarkers.clear()
-
-
-
-                busList.forEach { bus ->
-
-
-                    val array = bus.coordinatList[i].asJsonArray
-
-                    val longitude = array[0].asDouble / 1000000
-                    val latitude = array[1].asDouble / 1000000
-
-                    val markerOptions = MarkerOptions()
-
-                    markerOptions.position(LatLng(latitude, longitude))
-                        .title(bus.title)
-                        .icon(getMarkerIcon(bus, zoomLevel))
-                        .anchor(0.5F, 0.5F)
-
-                    val newMarker: Marker = mMap.addMarker(markerOptions)
-
-                    newMarker.tag = bus.id
-                    allMarkers.add(newMarker)
-                }
-
-                delay(updateRateInMillis)
-
-            }
-        } else {
-            bus_info.text = "Ingen busser kører på nuværende tidspunkt."
-            bus_info.visibility = View.VISIBLE
-            currentBusId = ""
-        }
-        if (currentBusId.isNotEmpty()) {
-            bus_info.text = busInfoGenerator(currentBusId)
-        }
-        viewModel.fetchBusLocations()
-    }
-
-
-    private fun routeCoordinates(coordinateList: List<LatLng>) {
-
-        if (polylineExist) {
-            polyLine.remove()
-        }
-        polyLine = mMap.addPolyline(
-            PolylineOptions().addAll(coordinateList).color(busColor)
-        )
-        polylineExist = true
-
-    }
-
-    private fun getMarkerIcon(bus: Bus, zoomLevel: Float): BitmapDescriptor? {
-        val markerView = CustomMarkerView(requireContext(), bus, zoomLevel, ::getColor)
-        return BitmapDescriptorFactory.fromBitmap(getBitmapFromView(markerView))
-    }
-
-    private fun getBitmapFromView(view: View): Bitmap? {
-        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
-        view.layout(0, 0, view.measuredWidth, view.measuredHeight)
-        var bitmap =
-            Bitmap.createBitmap(view.measuredWidth, view.measuredHeight, Bitmap.Config.ARGB_8888)
-        var canvas = Canvas(bitmap)
-        view.draw(canvas)
-        return bitmap
-    }
-
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -176,8 +76,11 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
         val bornholm = LatLng(55.125436, 14.919486)
         val zoom = 9.9F
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bornholm, zoom))
+        var zoomCheck = mMap.cameraPosition.zoom
+        var plotMarkerJob: Job? = null
+
         mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bornholm, zoom))
 
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
@@ -193,33 +96,9 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             mMap.isMyLocationEnabled = true
         }
 
-        mMap.setOnMapClickListener {
-            bus_info.visibility = View.INVISIBLE
-        }
-
-
-
-        mMap.setOnMarkerClickListener { marker ->
-
-            bus_info.visibility = View.VISIBLE
-            currentBusId = marker.tag.toString()
-
-            bus_info.text = busInfoGenerator(currentBusId)
-
-            cameraMove(marker.position)
-
-
-            busColor = getColor(marker.title)
-            viewModel.fetchBusRoute(marker.tag.toString())
-            true
-        }
-
-
         viewModel.routeCoordinates.observe(viewLifecycleOwner, Observer {
-            routeCoordinates(it.list)
+            plotRoute(it.list)
         })
-        var plotMarkerJob: Job? = null
-
 
         viewModel.liveBus.observe(viewLifecycleOwner, Observer {
             plotMarkerJob?.cancel()
@@ -233,14 +112,26 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         })
 
 
+        mMap.setOnMapClickListener {
+            bus_info.visibility = View.INVISIBLE
+        }
+
+        mMap.setOnMarkerClickListener { marker ->
+
+            bus_info.visibility = View.VISIBLE
+            currentBusId = marker.tag.toString()
+
+            bus_info.text = viewModel.busInfoGenerator(busList, currentBusId)
+
+            cameraMove(marker.position)
+
+            busColor = viewModel.getColor(requireContext(), marker.title)
+            viewModel.fetchBusRoute(marker.tag.toString())
+            true
+        }
 
         mMap.setOnCameraIdleListener {
             viewModel.fetchBusLocations()
-        }
-
-        var zoomCheck = mMap.cameraPosition.zoom
-        mMap.setOnCameraMoveCanceledListener {
-
         }
 
         mMap.setOnCameraMoveListener {
@@ -251,6 +142,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 zoomLevel = zoomCheck
             }
         }
+
 
 
         //moves compass button
@@ -272,6 +164,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+
     private fun cameraMove(position: LatLng) {
         if (zoomLevel < 10.5) {
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 10.5F))
@@ -280,119 +173,66 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
-    private fun busInfoGenerator(tag: String): String {
-        val builder = StringBuilder()
-        var result = ""
-        if (!busList.isEmpty()) {
-            busList.forEach { bus ->
-                if (bus.id == tag) {
-                    builder.append("Bus ").append(bus.title).append(" mod ").append(bus.destination)
-                        .append("\nNæste stop: ").append(bus.nextStop)
-                        .append(
-                            if (bus.delayInMins == "0" || bus.delayInMins.isEmpty()) {
-                                "\nKører til tiden"
-                            } else {
-                                "\nForsinkelse: "
-                            }
-                        ).append(
-                            if (bus.delayInMins == "0" || bus.delayInMins.isEmpty()) {
-                                ""
-                            } else {
-                                bus.delayInMins
-                            }
-                        ).append(
-                            if (bus.delayInMins == "0" || bus.delayInMins.isEmpty()) {
-                                ""
-                            } else if (bus.delayInMins == "1") {
-                                " Minut"
-                            } else {
-                                " Minutter"
-                            }
+    private fun plotRoute(coordinateList: List<LatLng>) {
 
-                        )
-                    result = builder.toString()
+        if (polylineExist) {
+            polyLine.remove()
+        }
+        polyLine = mMap.addPolyline(
+            PolylineOptions().addAll(coordinateList).color(busColor)
+        )
+        polylineExist = true
+    }
+
+    private suspend fun plotBusMarkers(busList: List<Bus>, zoomLevel: Float) {
+        if (busList.isEmpty()) {
+            bus_info.visibility = View.INVISIBLE
+            if (polylineExist) {
+                polyLine.remove()
+            }
+        }
+
+        if (busList.isNotEmpty()) {
+            for (i in 0 until busList[0].coordinatList.size() - 1) {
+                allMarkers.forEach { marker ->
+                    marker.remove()
                 }
+                allMarkers.clear()
+
+                busList.forEach { bus ->
+                    val array = bus.coordinatList[i].asJsonArray
+                    val longitude = array[0].asDouble / 1000000
+                    val latitude = array[1].asDouble / 1000000
+                    val markerOptions = MarkerOptions()
+
+                    markerOptions.position(LatLng(latitude, longitude))
+                        .title(bus.title)
+                        .icon(viewModel.getMarkerIcon(requireContext(), bus, zoomLevel))
+                        .anchor(0.5F, 0.5F)
+
+                    val newMarker: Marker = mMap.addMarker(markerOptions)
+
+                    newMarker.tag = bus.id
+                    allMarkers.add(newMarker)
+                }
+                delay(updateRateInMillis)
             }
+        } else {
+            bus_info.text = "Ingen busser kører på nuværende tidspunkt."
+            bus_info.visibility = View.VISIBLE
+            currentBusId = ""
         }
-        return result
+
+        if (currentBusId.isNotEmpty()) {
+            bus_info.text = viewModel.busInfoGenerator(busList, currentBusId)
+        }
+
+        viewModel.fetchBusLocations()
     }
 
-    private fun getColor(id: String): Int {
-
-        return when (id) {
-            "1" -> ContextCompat.getColor(requireContext(), R.color.Bus1and4)
-            "2" -> ContextCompat.getColor(requireContext(), R.color.Bus2)
-            "3" -> ContextCompat.getColor(requireContext(), R.color.Bus3and5)
-            "4" -> ContextCompat.getColor(requireContext(), R.color.Bus1and4)
-            "5" -> ContextCompat.getColor(requireContext(), R.color.Bus3and5)
-            "6" -> ContextCompat.getColor(requireContext(), R.color.Bus6)
-            "7" -> ContextCompat.getColor(requireContext(), R.color.Bus7and8)
-            "8" -> ContextCompat.getColor(requireContext(), R.color.Bus7and8)
-            "9" -> ContextCompat.getColor(requireContext(), R.color.Bus9)
-            "10" -> ContextCompat.getColor(requireContext(), R.color.Bus10)
-            "Færge" -> ContextCompat.getColor(requireContext(), R.color.Boat)
-            else -> ContextCompat.getColor(requireContext(), R.color.standard)
-        }
-    }
-
-    class CustomMarkerView(
-        context: Context,
-        bus: Bus,
-        zoomLevel: Float,
-        getColor: (id: String) -> Int
-    ) :
-        ConstraintLayout(context) {
-
-        init {
-            LayoutInflater.from(context).inflate(R.layout.custom_bus_marker, this, true)
-
-            val image = findViewById<ImageView>(R.id.bus_circle)
-            val title = findViewById<TextView>(R.id.bus_title)
-            val delay = findViewById<TextView>(R.id.bus_delay)
-
-            val minFactor = 0.78F
-            val maxFactor = 1.035F
-            val zoomFactor = zoomLevel / 12.5
-            title.textSize = 7F
-
-            val lp = image.layoutParams
-
-            val height = lp.height * zoomFactor
-            val width = lp.width * zoomFactor
-
-            if (maxFactor < zoomFactor) {
-                lp.height = (lp.height * maxFactor).toInt()
-                lp.width = (lp.width * maxFactor).toInt()
-                title.textSize = (title.textSize * maxFactor)
-            } else if (minFactor > zoomFactor) {
-                lp.height = (lp.height * minFactor).toInt()
-                lp.width = (lp.width * minFactor).toInt()
-                title.textSize = (title.textSize * minFactor)
-            } else {
-                lp.height = height.toInt()
-                lp.width = width.toInt()
-                title.textSize = (title.textSize * zoomFactor).toFloat()
-            }
-
-            image.layoutParams = lp
-            image.invalidate()
-
-            title.text = bus.title
-            image.setImageResource(bus.icon)
-            delay.text = bus.delayInMins
-
-            image.setColorFilter(getColor(bus.title))
 
 
-            if (bus.delayInMins == "0" || TextUtils.isEmpty(bus.delayInMins)) {
-                delay.visibility = GONE
-            } else {
-                delay.visibility = View.VISIBLE
-                delay.text = bus.delayInMins
-            }
 
-        }
-    }
 }
 
 private var zoomLevel = 10.5F
